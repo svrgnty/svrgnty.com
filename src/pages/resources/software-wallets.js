5b8e8fa30820e6b13b5f953c53fce5e830860155
svrgnty <120567975+svrgnty@users.noreply.github.com>
2023-01-30 12:48:15 +0100

    updates

---
+++ src/pages/resources/software-wallets.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+export const Head = () => (
+  <>
+    <title>Redirecting to https://svrgnty.com/resources/mobile-wallets-and-payment-apps/</title>
+    <meta http-equiv="refresh" content="0; URL=https://svrgnty.com/resources/mobile-wallets-and-payment-apps/" />
+  </>
+)
+
+const SoftwareWallets = () => (
+  <>
+    <link rel="canonical" href="https://svrgnty.com/resources/mobile-wallets-and-payment-apps/" />
+  </>
+);
+
+export default SoftwareWallets
