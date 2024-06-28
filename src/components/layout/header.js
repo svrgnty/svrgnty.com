47d6a14a93761ef6ab3ff9ef3eed7150f7fff9b2
svrgnty <120567975+svrgnty@users.noreply.github.com>
2024-06-28 12:59:34 +0200

    various fixes

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -9,11 +9,11 @@ const Header = () => (
   <header className="header">
     <div className="contain">
       <Link to="/" className="header-logo">
-        <img alt="" src={logo} /> Svrgnty.com
+        <img alt="" src={logo} /> svrgnty.com
       </Link>
-      <div className="share">
-        <Share />
-      </div>
+      {/* <div className="share">
+         <Share /> 
+      </div> */}
     </div>
   </header>
 
