8beb497fe00151b4ff26f334743e02bb8c94de65
svrgnty <120567975+svrgnty@users.noreply.github.com>
2024-09-16 10:38:50 +0200

    add live wallet

---
+++ src/pages/resources/coin-control.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Head = () => (
+  <>
+    <title>Redirecting to https://svrgnty.com/resources/utxo-management/</title>
+    <meta http-equiv="refresh" content="0; URL=https://svrgnty.com/resources/utxo-management/" />
+  </>
+)
+
+const CoinControl = () => (
+  <>
+    <link rel="canonical" href="https://svrgnty.com/resources/utxo-management/" />
+  </>
+);
+
+export default CoinControl
